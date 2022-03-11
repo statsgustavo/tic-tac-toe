3cac0ce30f9186a42f31f6fc97ebe8e8bbef0f23
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-11 17:42:14 -0300

    update: random-actions-choosing bot.

--- tic_tac_toe/agent.py
+++ tic_tac_toe/agent.py
@@ -4,25 +4,21 @@ from tic_tac_toe import game
 
 
 class Agent:
-
-    def __init__(self, game_board, epsilon=0.05, number_of_games_won=0):
+    def __init__(self, game_board):
         self._game_board = game_board
-        self._epsilon = epsilon
-        self._number_of_games_won = number_of_games_won
-
-    def _translate_board_to_string(self):
-        return "".join(self._game_board.ravel())
 
     def _get_available_positions_on_the_board(self):
         return np.argwhere(self._game_board == "-")
 
     def _update_board_knowledge(self, game_board):
         self._game_board = game_board
+        return self
 
     def play(self, game_board):
-        self._update_board_knowledge(game_board)
+        possible_moves = self._update_board_knowledge(
+            self, game_board
+        )._get_available_positions_on_the_board()
 
-        possible_moves = self._get_available_positions_on_the_board()
         move_idx = np.random.choice(possible_moves.shape[0], 1)[0]
         row, col = possible_moves[move_idx]
 
